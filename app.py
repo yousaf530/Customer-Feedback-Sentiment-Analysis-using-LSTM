3d366a82c9d4632094d566c46324dc1a62c65413
Muhammad Yousaf Saddique <yousafsaddique523@gmail.com>
2021-06-09 21:03:22 +0500

    Runtime bug fixed

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
+
 def init():
     global model,graph
-    graph = tf.compat.v1.get_default_graph()
-
+    graph = tf.Graph()
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -39,23 +40,11 @@ def sent_anly_prediction():
         tw = sequence.pad_sequences(tw,maxlen=200)
 
         
-        # word_to_id = imdb.get_word_index()
-        # strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
-        # text = text.lower().replace("<br />", " ")
-        # text=re.sub(strip_special_chars, "", text.lower())
-
-        # words = text.split() #split string into a list
-        # x_test = [[word_to_id[word] if (word in word_to_id and word_to_id[word]<=20000) else 0 for word in words]]
-        # x_test = sequence.pad_sequences(x_test, maxlen=200) # Should be same which you used for training data
-        # vector = np.array([x_test.flatten()])
-
+      
         
         with graph.as_default():
             # load the pre-trained Keras model
             model = load_model('sentiment_analysis.h5')
-
-            # probability = model.predict(np.array([vector][0]))[0][0]
-            # prediction = (model.predict(np.array([vector][0])) > 0.5).astype("int32")
 
             probability = model.predict(tw)[0][0]
             prediction = int(model.predict(tw).round().item())
@@ -69,6 +58,5 @@ def sent_anly_prediction():
     return render_template('index.html', text=text, sentiment=sentiment, probability=probability, image=img_filename)
 
 
-if __name__ == "__main__":
-    init()
-    app.run(debug=True)
+init()
+app.run(debug=True)
